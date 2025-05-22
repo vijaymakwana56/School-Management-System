730aafece4751b83860bd42548daa518a4fa1893
Vijay Makwana <vijaygm20@iitk.ac.in>
2025-05-22 19:58:38 +0530

    feat: parser

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const schoolRoutes = require("./routes/schoolRoutes");
 
 const app = express();
 
-app.use(express.urlencoded())
-// app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.use('/', schoolRoutes);
 
 app.listen(process.env.PORT, ()=>{
