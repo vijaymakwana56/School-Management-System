86b349ea564bbe36670ddc4fd5efdbeb6cfcf6c1
Vijay Makwana <vijaygm20@iitk.ac.in>
2025-05-22 18:22:40 +0530

    final commit

--- database.js
+++ database.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const database = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
+    port: process.env.DB_PORT,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
 });
